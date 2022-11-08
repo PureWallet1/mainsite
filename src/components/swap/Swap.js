bb52533ee07952626f27389d67fb3ad0fdb556e6
tusharnagarop <you@example.comtusharnagarop@gmail.com>
2022-11-08 12:49:55 +0530

    dapp fix

--- src/components/swap/Swap.js
+++ src/components/swap/Swap.js
@@ -2,25 +2,26 @@ import React from 'react';
 import BNBLogo from '../../images/swap/btc.svg'
 import ReceiveLogo from '../../images/swap/receive.svg'
 import SwitchIcon from '../../images/swap/switch.svg'
-import { SwapFunction, ApproveFunction } from './functions'
+import { CheckAllowance, ApproveFunction, SwapTokens } from './functions'
 import { ethers } from "ethers";
 import axios from "axios"
 import "./swap.scss"
-import values from "../../values.json";
-import {provider, setProvider, signer, setSigner} from '../../App';
-import tokenAbi from '../../abi/token.json';
+import toast, { Toaster } from 'react-hot-toast';
 
+const Swap = ({ useraddress, provider, wallet }) => {
 
-const Swap = ({ useraddress, provider }) => {
-  let _signer = React.useContext(signer);
   const [amount, setAmount] = React.useState("");
   const [receivingamount, setreceivingamount] = React.useState(0);
   const [swaptokentwo, SwapTokentwo] = React.useState("none");
   const [swaptokenone, SwapTokenone] = React.useState("");
   const [balancetoken, setBalanceToken] = React.useState(0);
   const [userbalance, setUserBalance] = React.useState(0);
+  const [tokenfromaddress, settokenfromaddress] = React.useState("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c");
+  const [tokentoaddress, settokentoaddress] = React.useState("0x1e8150ea46E2A7FBB795459198fBB4B35715196c");
   const [walletconnect, setWalletconnect] = React.useState(false);
   const [error, setError] = React.useState(false);
+  const [token, setToken] = React.useState("SHIH");
+
 
   setTimeout(function () {
     Wallet()
@@ -34,56 +35,127 @@ const Swap = ({ useraddress, provider }) => {
     if (useraddress === "Connect") {
       setWalletconnect(false)
     } else {
-      setWalletconnect(true)
+      toast.dismiss();
+      setWalletconnect(true); console.log("Wallet->", wallet)
       const balance = await provider.getBalance(useraddress);
       const balanceformat = ethers.utils.formatEther(balance);
-      setUserBalance(balanceformat);
-
-      try{
-        let puretoken = new ethers.Contract(
-          values.token,
-          tokenAbi,
-          _signer
-        );
-        let _wallet = await _signer.getAddress();  
-        console.log(_wallet)
-      const tokenbalance = await puretoken.balanceOf(_wallet);
-      const tokenbalanceformat = ethers.utils.formatEther(tokenbalance);
-      setBalanceToken(tokenbalanceformat)
+      let balanceendpoint = `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x1e8150ea46E2A7FBB795459198fBB4B35715196c&address=${useraddress}&tag=latest&apikey=SDVCH48GXWPXJEGA6J3J3UNRUV6JH997ZK`
+      try {
+        const response = await axios.get(balanceendpoint);
+        const balanceformat = ethers.utils.formatEther(response.data.result);
+        setBalanceToken(Math.round(balanceformat))
+      } catch (err) {
+        console.log("Error=>", err.message)
       }
-      catch(err){
-        console.log(err)
-      }
-
-
-      await SwapFunction(useraddress)
+      setWalletconnect(true);
+      setUserBalance(balanceformat)
+      await CheckAllowance(useraddress, tokentoaddress)
     }
   }
 
+
   const Connectwallet = async () => {
+    toast.loading("Connecting Wallet...")
     document.getElementById("walletconnect").click();
   }
 
 
-  const Tokenvalue = async (amount) => {
-    if (amount > userbalance) {
+  const Tokenvalue = async (props) => {
+    if (walletconnect === true) {
+      if (tokenfromaddress === "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c") {
+    if (props > userbalance) {
+      const qs = require('qs');
+      document.querySelector('#swapBtn').disabled = true;
       setError("Insufficient BNB balance")
+       try {
+        const params = {
+          "sellToken": tokenfromaddress,
+          "buyToken": tokentoaddress,
+          "sellAmount": (ethers.utils.parseEther(props)).toString(),
+        }
+        const response = await fetch(`https://bsc.api.0x.org/swap/v1/price?${qs.stringify(params)}`);
+        const data = await response.json();
+        const price = ethers.utils.formatEther(data.buyAmount);
+        const result = Math.round(price * 100000) / 100000;
+        setreceivingamount(result)
+      } catch (error) {
+        setError("Insufficient BNB balance")
+      }
     }
     else {
-    let amountconvert = ethers.utils.parseEther(amount);
-    let exchangerootendpoint = `https://api.1inch.exchange/v4.0/56/quote?fromTokenAddress=0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c&toTokenAddress=0xc3BcE47886e56316B2A5A4b2C926561AE94039A2&amount=${amountconvert}`
-    try {
-      const response = await axios.get(exchangerootendpoint);
-      //console.log("Approval=>", response.data)
-    } catch (exchangerootendpoint) {
-      let error = exchangerootendpoint.response.data.description;
-      setError(error)
+      const qs = require('qs');
+      document.querySelector('#swapBtn').disabled = false;
+      try {
+        const params = {
+          "sellToken": tokenfromaddress,
+          "buyToken": tokentoaddress,
+          "sellAmount": (ethers.utils.parseEther(props)).toString(),
+        }
+        const response = await fetch(`https://bsc.api.0x.org/swap/v1/price?${qs.stringify(params)}`);
+        const data = await response.json();
+        const price = ethers.utils.formatEther(data.buyAmount);
+        const result = Math.round(price);
+        setreceivingamount(result)
+      } catch (error) {
+        //
+      }
     }
+  }
+  else {
+    if (props > balancetoken) {
+      const qs = require('qs');
+      document.querySelector('#swapBtn').disabled = true;
+      setError("Insufficient PURE balance")
+       try {
+        const params = {
+          "sellToken": tokenfromaddress,
+          "buyToken": tokentoaddress,
+          "sellAmount": (ethers.utils.parseEther(props)).toString(),
+        }
+        const response = await fetch(`https://bsc.api.0x.org/swap/v1/price?${qs.stringify(params)}`);
+        const data = await response.json();
+        const price = ethers.utils.formatEther(data.buyAmount);
+        const result = Math.round(price * 100000) / 100000;
+        setreceivingamount(result)
+        document.querySelector('#swapBtn1').disabled = true;
+      } catch (error) {
+        setError("Insufficient PURE balance")
+      }
+    }
+    else {
+      const qs = require('qs');
+      document.querySelector('#swapBtn').disabled = false;
+      try {
+        const params = {
+          "sellToken": tokenfromaddress,
+          "buyToken": tokentoaddress,
+          "sellAmount": (ethers.utils.parseEther(props)).toString(),
+        }
+        const response = await fetch(`https://bsc.api.0x.org/swap/v1/price?${qs.stringify(params)}`);
+        const data = await response.json();
+        const price = ethers.utils.formatEther(data.buyAmount);
+        const result = Math.round(price);
+        setreceivingamount(result)
+      } catch (error) {
+        //
+      }
+    }
+  }
+  setTimeout(function () {
+  setError(false)
+  }, 3000);
+  }
+  else {
+    toast.error("Please connect wallet")
   }
   }
 
   return (
     <div className='landing'>
+      <Toaster
+      position="top-center"
+      reverseOrder={false}
+      />
       <div className='stak_box'>
         <div className='stak_heading'>
           <h2>Swapping</h2>
@@ -92,13 +164,13 @@ const Swap = ({ useraddress, provider }) => {
         </div>
         <div className='stak_body' id="section-one" style={{ display: `${swaptokenone}` }}>
           <div className="input1">
-            <input placeholder='Enter Amount' value={amount}
-              onChange={(e) => { setAmount(e.target.value); Tokenvalue(amount) }} className="form-field" type="text" />
+            <input placeholder='Enter Amount'
+              onChange={(e) => { setAmount(e.target.value); Tokenvalue(e.target.value) }} className="form-field" type="text" />
             <div className='maxToken'>
               <img src={BNBLogo} alt="BNB Logo" width="100%" />
             </div>
           </div>
-          <div onClick={() => { SwapTokentwo(""); SwapTokenone("none") }} className="switciconlogo">
+          <div onClick={() => { setreceivingamount(""); setAmount(""); setToken("BNB"); SwapTokentwo(""); SwapTokenone("none"); settokenfromaddress("0x1e8150ea46E2A7FBB795459198fBB4B35715196c"); settokentoaddress("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c") }} className="switciconlogo">
             <img src={SwitchIcon} alt="Switch Icon" />
           </div>
 
@@ -114,20 +186,20 @@ const Swap = ({ useraddress, provider }) => {
             </div>
           ) : walletconnect === true ? (
             <div className="switciconlogo">
-              <button type='submit' onClick={() => ApproveFunction(useraddress, amount)} className='swap-button'>SWAP</button>
+              <button id="swapBtn" type='submit' onClick={() => SwapTokens(useraddress, amount, tokenfromaddress, tokentoaddress)} className='swap-button'>SWAP</button>
             </div>
           ) : ("")}
         </div>
 
         <div className='stak_body' id="section-two" style={{ display: `${swaptokentwo}` }}>
           <div className="input1">
-            <input placeholder='Enter Amount' value={amount}
-              onChange={(e) => { setAmount(e.target.value); Tokenvalue(amount) }} className="form-field" type="text" />
+            <input placeholder='Enter Amount'
+              onChange={(e) => { setAmount(e.target.value); Tokenvalue(e.target.value) }} className="form-field" type="text" />
             <div className='maxToken'>
               <img src={ReceiveLogo} alt="Recever Logo" width="100%" />
             </div>
           </div>
-          <div onClick={() => { SwapTokentwo("none"); SwapTokenone("") }} className="switciconlogo">
+          <div onClick={() => {  setreceivingamount(""); setToken("SHIH"); setAmount(""); SwapTokentwo("none"); SwapTokenone(""); settokenfromaddress("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"); settokentoaddress("0x1e8150ea46E2A7FBB795459198fBB4B35715196c") }} className="switciconlogo">
             <img src={SwitchIcon} alt="Switch Icon" />
           </div>
           <div className="input1">
@@ -142,7 +214,7 @@ const Swap = ({ useraddress, provider }) => {
             </div>
           ) : walletconnect === true ? (
             <div className="switciconlogo">
-              <button type='submit' onClick={() => ApproveFunction(useraddress, amount)} className='swap-button'>SWAP</button>
+              <button id='swapBtn1' type='submit' onClick={() => SwapTokens(useraddress, amount, tokenfromaddress, tokentoaddress)} className='swap-button'>SWAP</button>
             </div>
           ) : ("")}
 
@@ -157,7 +229,7 @@ const Swap = ({ useraddress, provider }) => {
 
         <div className='stak_body_footer'>
           <p>Your BNB Balance <span className='stack_value'>{userbalance} BNB</span></p>
-          <p>Your PURE Balance <span className='stack_value'>{parseFloat(balancetoken).toFixed(2)} PURE</span></p>
+          <p>Your PURE Balance <span className='stack_value'>{balancetoken} PURE</span></p>
         </div>
       </div>
     </div >
--- src/components/swap/functions.js
+++ src/components/swap/functions.js
@@ -1,12 +1,194 @@
-import axios from "axios"
+import axios from "axios";
+import { ethers, providers } from "ethers";
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import toast from 'react-hot-toast';
 
-export const FetchToken = async (walletaddress) => { }
 
-export const SwapFunction = async (walletaddress) => {
-   
+
+export const FetchToken = async () => {
+    /* Step 1 : Lookup addresses of tokens you want to swap */
+    let response = await fetch('https://api.1inch.exchange/v4.0/56/tokens');
+    let tokenListJSON = await response.json();
+    //console.log("Look Up Address=> ", tokenListJSON);
 }
 
-export const ApproveFunction = async (walletaddress, amount) => {
+export const CheckAllowance = async (walletaddress, tokentoaddress) => {
+    /* Step 2 : Check for allowance of 1inch router contract to spend source asset */
+    let allowanceendpoint = `https://api.1inch.exchange/v4.0/56/approve/allowance?tokenAddress=${tokentoaddress}&walletAddress=${walletaddress}`
+    try {
+        const response = await axios.get(allowanceendpoint);
+        //console.log("Allowance=>", response.data)
+    } catch (err) {
+        //console.log("Error=>", err.message)
+    }
+    return;
+}
+
+export const ApproveFunction = async (walletaddress, amount, tokenfromaddress, tokentoaddress, wallet) => {
+    /* Step 3 : If necessary, give approval for 1inch router to spend source token */
+    let loading = toast.loading("Please Wait until the contract gets approved")
+    const prices = ethers.utils.parseUnits(amount.toString(), 'ether')
+    let approvalendpoint = `https://api.1inch.exchange/v4.0/56/approve/transaction?tokenAddress=${tokentoaddress}&amount=${prices.toNumber()}`
+    try {
+        const response = await axios.get(approvalendpoint);
+        console.log("Response=>", response)
+        /* If the User connction wallet is metamask the function will execute */
+        if (wallet === "wallet") {
+            try {
+                const sendmetamask = response.data;
+                try {
+                    const { ethereum } = window;
+                    await ethereum.request({ method: "eth_requestAccounts" });
+                    const accounts = await ethereum.request({ method: "eth_accounts" });
+                    const chainId = await ethereum.request({ method: "eth_chainId" });
+                    sendmetamask.from = accounts[0];
+                    sendmetamask.gasPrice = "2000000000";
+                    try {
+                        const transaction = await window.ethereum.request({
+                            method: "eth_sendTransaction",
+                            params: [sendmetamask],
+                        });
+                        console.log('Response Meta=>', transaction);
+                        toast.success("Transaction Approved, Please Wait...")
+                        SwapTokens(walletaddress, amount, tokenfromaddress, tokentoaddress, wallet)
+                    } catch (error) {
+                        toast.dismiss(loading)
+                        console.log("Error=>", error)
+                    }
+                }
+                catch (error) {
+                    toast.dismiss(loading)
+                    console.log("Error=>", error)
+                }
+
+            } catch (err) {
+                toast.dismiss(loading)
+                console.log("Error=>", err.message)
+            }
+        } else {
+            /*If the wallet not equal to metamask it goes to wallet connect function */
+            const sendmetamask = response.data;
+            const connector = new WalletConnect({
+                bridge: "https://bridge.walletconnect.org", // Required
+                qrcodeModal: QRCodeModal,
+            });
+            if (!connector.connected) {
+                // create new session
+                connector.createSession();
+                connector.on("connect", (error, payload) => {
+                    if (error) {
+                        throw error;
+                    }
+                    // Get provided accounts and chainId
+                    const { accounts, chainId } = payload.params[0];
+                    const account = accounts[0]
+                    console.log(account);
+                    alert("Please Click again to Initiate the Transaction...")
+                });
+            }
+            else {
+                sendmetamask.from = walletaddress;
+                connector.sendTransaction(sendmetamask).then((swapresult) => {
+                    console.log('Swap Result', swapresult);
+                })
+                    .catch((error) => {
+                        toast.dismiss(loading)
+                        console.error('sell error', error);
+                    });
+            }
+        }
+        toast.success("Transaction Approved, Please Wait...")
+        toast.dismiss(loading)
+    } catch (err) {
+        toast.dismiss(loading)
+        //console.log("Error=>", err.message)
+    }
+}
+
+export const SwapTokens = async (walletaddress, amount, tokenfromaddress, tokentoaddress, wallet) => {
+    const qs = require('qs');
+    toast.loading("Initiating Transaction...")
+    
+        /* Step 5 : All Success ready use to perform swap */
+        const params = {
+            "sellToken": tokenfromaddress,
+            "buyToken": tokentoaddress,
+            "sellAmount": (ethers.utils.parseEther(amount)).toString(),
+          }  
+        const response = await fetch(`https://bsc.api.0x.org/swap/v1/quote?${qs.stringify(params)}`);
+        const data = await response.json();
+        /* If the User connction wallet is metamask the function will execute */
+        if (wallet === "wallet") {
+            try {
+                const sendmetamask = data;
+                try {
+                    toast.dismiss()
+                    let loading = toast.loading("Please Wait until the contract gets approved")
+                    const { ethereum } = window;
+                    await ethereum.request({ method: "eth_requestAccounts" });
+                    const accounts = await ethereum.request({ method: "eth_accounts" });
+                    const chainId = await ethereum.request({ method: "eth_chainId" });
+                    sendmetamask.from = accounts[0];
+                    sendmetamask.gasPrice = "2000000000";
+                    try {
+                        const transaction = await window.ethereum.request({
+                            method: "eth_sendTransaction",
+                            params: [sendmetamask],
+                        });
+                        console.log('Response Meta=>', transaction);
+                        toast.dismiss(loading)
+                        toast.success("Swap Transaction Success....");
+                    } catch (error) {
+                        console.log("Error=>", error)
+                        toast.dismiss(loading)
+                        toast.error("Swap Transaction Failed....");
+                    }
+                }
+                catch (error) {
+                    console.log("Error=>", error)
+                }
+
+            } catch (err) {
+                console.log("Error=>", err.message)
+            }
+        } else {
+            /*If the wallet not equal to metamask it goes to wallet connect function */
+            toast.dismiss()
+            let loading = toast.loading("Please Wait until the contract gets approved")
+            const sendmetamask = data;
+            const connector = new WalletConnect({
+                bridge: "https://bridge.walletconnect.org", // Required
+                qrcodeModal: QRCodeModal,
+            });
+            if (!connector.connected) {
+                // create new session
+                connector.createSession();
+                connector.on("connect", (error, payload) => {
+                    if (error) {
+                        throw error;
+                    }
+                    // Get provided accounts and chainId
+                    const { accounts, chainId } = payload.params[0];
+                    const account = accounts[0]
+                    console.log(account);
+                    alert("Please Click again to Initiate the Transaction...")
+                });
+            }
+            else {
+                sendmetamask.from = walletaddress;
+                await connector.sendTransaction(sendmetamask).then((swapresult) => {
+                    console.log('Swap Result', swapresult);
+                    toast.success("Swap Transaction Success....");
+                    toast.dismiss(loading)
+                })
+                    .catch((error) => {
+                        console.error('sell error', error);
+                        toast.dismiss(loading)
+                        toast.error("Swap Transaction Failed....");
+                    });
+            }
+            
+        }
     
 }
-
